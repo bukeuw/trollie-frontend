60e42a0fe86e576fa352680be0552bbf7235a2bd
Santo Blasius <sntblasius@gmail.com>
2021-04-15 13:41:49 +0700

    Add component for sidebar

---
+++ components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-nav vertical>
+    <b-nav-item to="/">
+      Boards
+    </b-nav-item>
+    <b-nav-item to="/">
+      Template
+    </b-nav-item>
+    <b-nav-item to="/">
+      Home
+    </b-nav-item>
+  </b-nav>
+</template>
