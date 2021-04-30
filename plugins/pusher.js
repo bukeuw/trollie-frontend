f0999abb703706257f08eefb304e3a07c90e0f98
Santo Blasius <sntblasius@gmail.com>
2021-04-30 11:59:58 +0700

    reload the token when user logged in

--- plugins/pusher.js
+++ plugins/pusher.js
@@ -2,7 +2,7 @@ import Pusher from 'pusher-js'
 import Cookie from 'js-cookie'
 
 export default (context, inject) => {
-  const authToken = Cookie.get('auth._token.laravelJWT') || ''
+  let authToken = Cookie.get('auth._token.laravelJWT') || ''
   const { key, cluster } = context.$config.pusher
   const { browserBaseURL } = context.$config.axios
   const options = {
@@ -20,17 +20,11 @@ export default (context, inject) => {
   const channel = pusher.subscribe('presence-activities')
 
   channel.bind('pusher:subscription_error', ({ status }) => {
-    // eslint-disable-next-line
-    console.log('error subscribing' + status)
-
     if (status === 401) {
+      authToken = Cookie.get('auth._token.laravelJWT') || ''
       pusher.config.auth.headers.Authorization = authToken
       channel.subscribe('presence-activities')
     }
-  })
-  channel.bind('pusher:subscription_succeeded', () => {
-    // eslint-disable-next-line
-    console.log('subscribed')
   })
 
   inject('pusher', pusher)
